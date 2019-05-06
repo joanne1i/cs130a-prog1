bab68b69db803cc4ba01df9e7d72eeee70a722c5
Joanne <joanneli@ucsb.edu>
2019-05-06 12:01:46 -0700

    updated hash

--- HashSet.cpp
+++ HashSet.cpp
@@ -3,11 +3,11 @@
 HashSet::HashSet() {
 	this->nitems = 0;
 	// will have to resize when nitems == nslots
-	this->nslots = 100000;
+	this->nslots = 1000;
 	this->intfn = new SquareRootHash(0, nslots);
 	this->strfn = new JenkinsHash();
 	this->strfn2 = new PearsonHash();
-	this->slots = new std::string*[nslots];
+	this->slots = new std::string*[nslots]();
 }
 
 HashSet::~HashSet() {
@@ -18,16 +18,21 @@ HashSet::~HashSet() {
 // re-insert every item
 void HashSet::rehash() {
 	int index;
+	int input;
 	this->nslots = 2*nslots;
-	this->nitems = 0;
-	std::string** temp = new std::string*[nslots];
+	std::string** temp = new std::string*[nslots]();
+	delete intfn;
 	// get new integer hash function
 	this->intfn = new SquareRootHash(0, nslots);
 	// re-inserts every item in new table
-	for(int i = 0; i < sizeof(slots); i++) {
-		while(slots[i] != NULL) {
-			index = intfn->hash(slots[i]);
-			temp[index] = *slots[i];
+	for(int i = 0; i < nslots/2; i++) {
+		if(slots[i] != NULL) {
+			input = strfn->hash(*slots[i]);
+			index = intfn->hash(input);
+			// collision check
+			while(temp[index] != NULL) 
+				index = intfn->hash(index+1);
+			temp[index] = slots[i];
 		}
 	}
 	delete[] slots;
@@ -41,8 +46,11 @@ void HashSet::insert(const std::string& value) {
 	uint64_t index = intfn->hash(input);
 	if(lookup(value) == false) {
 		nitems++;
-		if(nslots == nitems) rehash();
-		// linear probing
+		if(nslots == nitems) {
+		       	rehash();
+			index = intfn->hash(input);
+		}
+		// probing
 		while(slots[index] != NULL)
 			index = intfn->hash(input+1);
 		*slots[index] = value;
